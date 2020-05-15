f73cad14e8a460586991eea8a3175564e1fe69f7
Debanshu Datta <debanshudatta123@gmail.com>
2020-05-16 02:33:34 +0530

    Post layout/PostHolder/PostAdpter is set

---
+++ app/src/main/java/com/example/androidretrofit2rejava/Adapter/PostAdapter.java
@@ -0,0 +1,47 @@
+package com.example.androidretrofit2rejava.Adapter;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidretrofit2rejava.Model.Post;
+import com.example.androidretrofit2rejava.R;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostViewHolder>{
+
+    Context context;
+    List<Post> postList;
+
+    public PostAdapter(Context context, List<Post> postList) {
+        this.context = context;
+        this.postList = postList;
+    }
+
+    @NonNull
+    @Override
+    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.post_layout,parent,false);
+        return new PostViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+      holder.txt_author.setText(String.valueOf(postList.get(position).userId));
+        holder.txt_title.setText(String.valueOf(postList.get(position).title));
+        holder.txt_content.setText(new StringBuilder(postList.get(position).title.substring(0,20))
+        .append("...").toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+}
